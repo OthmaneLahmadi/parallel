703d18f08db5209f59344f4b801ebcfcb41f902d
lahmadi_othmane <otmanwacreal20765@gmail.com>
2023-12-03 14:20:12 +0100

    version 2.1 generic makefile

--- simple.c
+++ simple.c
@@ -7,7 +7,6 @@ MPI_Init(NULL,NULL);
 int rank,size;
 MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 MPI_Comm_size(MPI_COMM_WORLD,&size);
-printf("\n12dede\n");
 printf("process %d the number of cpu %d\n",rank,size);
 MPI_Finalize();
 return 0;
